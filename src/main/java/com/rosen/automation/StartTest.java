e1bac7c529392fdd5fcf1dbf95dfa414c88ca42b
rosko89 <rrusenov@qualityhouse.biz>
2015-06-04 17:25:04 +0300

    small changes in StartTest.java

--- src/main/java/com/rosen/automation/StartTest.java
+++ src/main/java/com/rosen/automation/StartTest.java
@@ -1,16 +1,29 @@
 package com.rosen.automation;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import com.gargoylesoftware.htmlunit.javascript.host.Console;
+import com.rosen.automation.pageobjects.Browsers;
 import com.rosen.automation.pageobjects.HomePage;
 import com.rosen.automation.properties.PropertiesCache;
 
 public class StartTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedURLException {
 		//PropertiesCache read=new PropertiesCache();
 		
-		PropertiesCache prop = PropertiesCache.getInstance();
-		System.out.println(prop.getProperty(""));
-		HomePage homePage=new HomePage();
+		//PropertiesCache prop = PropertiesCache.getInstance();
+		//System.out.println(prop.getProperty(""));
+		//HomePage homePage=new HomePage(Browsers.SAFARI);
+		DesiredCapabilities capability = DesiredCapabilities.firefox();
+		System.out.println(capability.getCapability(""));
+		//WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), capability);
+		//driver.get("http://www.gol.bg");
 	}
 
 }
